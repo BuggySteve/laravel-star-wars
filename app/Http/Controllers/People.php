c1415a773ea5e6738f22f187ed672e4920f9e2a7
StevenLindelauff <steven@immense.nl>
2020-10-27 21:57:55 +0100

    added constructor function to people controller

--- app/Http/Controllers/People.php
+++ app/Http/Controllers/People.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class People extends Controller
+class People extends ApiResourceController
 {
-    //
+    public function __construct()
+    {
+        $this->url = config('swapi.swapi_url').'people';
+        $this->type = 'people';
+    }
 }
