06124b079c02bb60628e15a373d2946d74afaf30
StevenLindelauff <steven@immense.nl>
2020-10-27 22:50:52 +0100

    added necessary vars to index view

--- app/Http/Controllers/ApiResourceController.php
+++ app/Http/Controllers/ApiResourceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Exception;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 abstract class ApiResourceController extends Controller
@@ -14,12 +15,21 @@ abstract class ApiResourceController extends Controller
      * Display a listing of the items
      */
     public function index(Request $request) {
-        $view = $this->type . '.index';
+        $page = $request->query('page', '');
+        $type = $this->type;
+
+        if ($page != '') {
+            $this->url .= '?page=' . $page;
+        }
 
         try {
-            $items = $this->fetchData($this->url);
+            $resultData = $this->fetchData($this->url);
+            $pagination = $this->getPagination($resultData);
+            $next = $pagination['next'];
+            $previous = $pagination['previous'];
+            $items = $resultData['results'];
 
-            return view($view, compact('items'));
+            return view('index', compact('items', 'next', 'previous', 'type'));
         } catch(Exception $exception) {
             // throw new Exception("Could not fetch ".$type." from the STRAPI...", $exception);
             throw $exception;
@@ -39,5 +49,13 @@ abstract class ApiResourceController extends Controller
         } catch(Exception $exception) {
             throw $exception;
         }
+    }
+
+    private function getPagination($result) {
+        return [
+            'next' => isset($result['next']) ? Str::after($result['next'], 'page=') : '',
+            'previous' => isset($result['previous']) ? Str::after($result['previous'], 'page=') : ''
+        ];
+
     }
 }
