87293247e1701c911e8e4fc2f65ba4093761cb21
StevenLindelauff <steven@immense.nl>
2020-10-28 14:06:58 +0100

    added migration for person species pivot table

---
+++ database/migrations/2020_10_28_130324_create_person_species_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonSpeciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('person_species', function (Blueprint $table) {
+            $table->unsignedInteger('person_id');
+            $table->unsignedInteger('species_id');
+
+            $table->primary(['person_id', 'species_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('person_species');
+    }
+}
