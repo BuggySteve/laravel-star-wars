b867c6b046041b8d2ebd7ae106834e79602c844e
StevenLindelauff <steven@immense.nl>
2020-10-28 17:28:43 +0100

    added show method to species controller

--- app/Http/Controllers/SpeciesController.php
+++ app/Http/Controllers/SpeciesController.php
@@ -11,4 +11,25 @@ class SpeciesController extends ApiResourceController
         $this->url = config('swapi.swapi_url').'species';
         $this->type = 'species';
     }
+
+    public function show($id) {
+        $individualUrl = $this->url.'/'.$id;
+        $species = $this->fetchData($individualUrl);
+
+        $i = 0;
+        foreach ($species['people'] as $item) {
+            $species['people'][$i] = $this->fetchData($item);
+            $i++;
+        }
+
+        $i = 0;
+        foreach ($species['films'] as $item) {
+            $species['films'][$i] = $this->fetchData($item);
+            $i++;
+        }
+
+        $item = $species;
+
+        return view('show', compact('item'));
+    }
 }
