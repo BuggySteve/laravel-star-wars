9cad0c1585df9322ea3f9ca3bff541f8b9f89b4f
StevenLindelauff <steven@immense.nl>
2020-10-28 16:39:14 +0100

    personcontroller show method

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Person;
 use Illuminate\Http\Request;
 
 class PersonController extends ApiResourceController
@@ -10,5 +11,21 @@ class PersonController extends ApiResourceController
     {
         $this->url = config('swapi.swapi_url').'people';
         $this->type = 'people';
+    }
+
+    public function show($id) {
+        $individualUrl = $this->url.'/'.$id;
+        $person = $this->fetchData($individualUrl);
+        $person['homeworld'] = $this->fetchData($person['homeworld']);
+
+        $i = 0;
+        foreach ($person['species'] as $item) {
+            $person['species'][$i] = $this->fetchData($item);
+            $i++;
+        }
+
+        $item = $person;
+
+        return view('show', compact('item'));
     }
 }
